e7d6bbdc562dbfaaad07a79624b1002dc87ce4f4
Abhay056 <77850911+Abhay056@users.noreply.github.com>
2025-08-01 22:42:26 +0530

    Some bug fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import { AuthProvider } from '../context/AuthContext';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
